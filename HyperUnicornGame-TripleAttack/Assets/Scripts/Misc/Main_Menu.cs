465b6076f52cc1cb5cd7b4f3234a0a79c5b67f65
Michelle <j.s.frankfort@gmail.com>
2023-04-29 17:34:11 +0200

    Added a simple main menu

---
+++ HyperUnicornGame-TripleAttack/Assets/Scripts/Misc/Main_Menu.cs
@@ -0,0 +1,48 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+using UnityEngine.UI;
+
+public class Main_Menu : MonoBehaviour
+{
+
+    Button play_game, how_To, quit;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        play_game.onClick.AddListener(PlayGame);
+        how_To.onClick.AddListener(HowTo);
+        quit.onClick.AddListener(QuitGame);
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        ExitGame();
+    }
+
+    void PlayGame()
+    {
+        SceneManager.LoadScene(sceneBuildIndex:  1);
+    }
+
+    void HowTo()
+    {
+        SceneManager.LoadScene(sceneBuildIndex: 2);
+    }
+
+    void QuitGame()
+    {
+        Application.Quit();
+    }
+
+    void ExitGame()
+    {
+        if (Input.GetButtonDown("Cancel"))
+        {
+            QuitGame();
+        }
+    }
+}
